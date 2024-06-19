e86505f3edee17ba0812f1585e47f83e9a1ec06f
Eduard <eduard.lisovskij@gmail.com>
2024-06-19 16:52:06 +0200

    add card, fetch and pegination functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -7,8 +9,52 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+const searchBarInput = document.querySelector("#searchBarInput");
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+const maxPage = 42;
+let page = 1;
+let searchQuery = "";
+const url = "https://rickandmortyapi.com/api/character/?page=";
+
+pagination.textContent = `${page} / ${maxPage}`;
+
+async function fetchCharacters(page) {
+  const response = await fetch(url + page);
+  const character = await response.json();
+
+  console.log(character);
+
+  cardContainer.innerHTML = "";
+
+  character.results.forEach((element) => {
+    CharacterCard(element);
+  });
+}
+fetchCharacters(1);
+
+prevButton.addEventListener("click", () => {
+  if (page === 1) {
+    return;
+  }
+  fetchCharacters(page - 1);
+  page--;
+  pagination.textContent = `${page} / ${maxPage}`;
+});
+
+nextButton.addEventListener("click", () => {
+  if (page === maxPage) {
+    return;
+  }
+  fetchCharacters(page + 1);
+  page++;
+  pagination.textContent = `${page} / ${maxPage}`;
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = searchBarInput.value;
+  const searchURL = url + `&name=${searchQuery}`;
+  console.log(searchURL);
+  fetchCharacters();
+});
